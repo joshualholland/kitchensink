c5c350beed0fe893fd7b3edad96f931ad70bc176
joshualholland <joshualholland@gmail.com>
2018-10-04 16:43:16 -0500

    Hello World

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+// name variable
 let name = 'Josh';
-
+// constant variables
 const states = 50;
-
+// Basic addition
 let x = 5 + 4;
 console.log(x);
-
+// If first letter is before or after L in the alpahbet
 let code = name.charCodeAt(0) 
 if (name.charCodeAt(0) > 76) {
     console.log("Back of the Line!")
 } else {
     console.log("Next!")
 };
+// hello world
+function sayHello(alert) {
+    alert("Hello World!")
+};
+alert("Hello World!");
