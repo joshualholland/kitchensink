9c8fc11f0aff8082622a3bba053361d738cff545
joshualholland <joshualholland@gmail.com>
2018-10-04 16:07:04 -0500

    step 6

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const states = 50;
 
 let x = 5 + 4;
 console.log(x);
+
+let code = name.charCodeAt(0) 
+if (name.charCodeAt(0) > 76) {
+    console.log("Back of the Line!")
+} else {
+    console.log("Next!")
+};
