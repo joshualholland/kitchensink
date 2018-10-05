30f0c8268fa0401a18cec436c02d0e9886af0433
joshualholland <joshualholland@gmail.com>
2018-10-05 15:14:57 -0500

    almost!

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ function sayHello(alert) {
 alert("Hello World!");
 // Check age
 
-function checkAge(person, age) {
-    if (age < 21)
-    alert("Sorry ${James} you aren't old enough to view this page")
+function checkAge(fname,age) {
+    if (fname, age < 21)
+    alert("Sorry ${fname} you aren't old enough to view this page")
 };
 checkAge('Charles', 21);
 checkAge('Abby', 27);
@@ -32,3 +32,24 @@ let veggies = ['squash', 'ocra', 'radishes', 'carrots'];
 for(let i = 0; i < veggies.length; i++) {
     console.log(veggies[i]);
 }
+// name and age array and loops
+let age = [ 
+    { fname:'Joel', age: 23 },
+    { fname:'Devin', age: 27 },
+    { fname:'Jordan', age: 20 },
+    { fname:'Nathan', age: 14 },
+    { fname:'Jeff', age: 44 }
+];
+for(let i = 0; i < age.length; ++i) {
+   console.log(checkAge(age))
+};
+// get length
+let word = 'Hello World';
+console.log(getLength(word.length))
+function getLength(word) {
+    return(word)
+};
+getLength('Hello World');
+
+let length = 11;
+console.log("The world is an odd place!");
